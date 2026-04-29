f5ac0a35d44f7cae0fc20ab3ee61536fbad6e201
s.mustafa <saidbekmustafayev@gmail.com>
2026-04-29 09:26:04 +0500

    29.04.26

--- mysite/myresume/views.py
+++ mysite/myresume/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 
 
 def index(request: HttpRequest):
+    products = [
+        ('laptop', 2199),
+        ('desktop', 3000),
+        ('phone', 500)
+    ]
     context = {
         "time_run": default_timer(),
+        'products': products
     }
 
     return render(request, "myresume/myresume-index.html", context=context)
